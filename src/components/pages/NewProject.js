35e98d5b7c1fb9846b6a6bd950f645e35b3a507a
Kevin Souto <kevinsoutods@gmail.com>
2022-07-08 20:19:30 -0300

    Criando o Form do projeto

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,7 +1,15 @@
 import React from "react";
+import ProjectForm from "../project/ProjectForm";
+import styles from './NewProject.module.css';
 
 function NewProject() {
-    return <h1>NewProject</h1>
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie o projeto para depois adicionar os serviços.</p>
+            <ProjectForm />
+        </div>
+    );
 }
 
 export default NewProject;
