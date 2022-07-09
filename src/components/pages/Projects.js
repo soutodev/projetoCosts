9ef9f1b994e65f1af39622f60a469881fa8bcc8d
Kevin Souto <kevinsoutods@gmail.com>
2022-07-09 20:47:12 -0300

    Resgatando dados da API - JsonServer

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,18 +1,37 @@
 import React from "react";
 import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react'
+
 import Message from "../layout/Message";
 import Container from '../layout/Container';
 import LinkButton from '../layout/LinkButton';
+import ProjectCard from "../project/ProjectCard";
 
 import styles from './Projects.module.css';
 
 function Projects() {
 
+    const [projects, setProjects] = useState([]);
+    
     const location = useLocation();
     let message = '';
     if (location.state) {
         message = location.state.message
     }
+
+    useEffect(() => {
+        fetch("http://localhost:5000/projects", {
+            method: 'GET',
+            headers: {
+                'Content-Type':'application/json',
+            },
+        }).then((resp) => resp.json())
+            .then( (data) => {
+                console.log(data);
+                setProjects(data)
+            })
+          .catch((err) => console.log(err));
+    }, []);
 
     return (
         <div className={styles.project_container}>
@@ -24,7 +43,17 @@ function Projects() {
             {message && <Message type="success" msg={message} />}
             
             <Container customClass="start">
-                <p>Projetos...</p>
+                {projects.length > 0 && 
+                    projects.map((project) => (
+                        <ProjectCard 
+                            name={project.name}
+                            id={project.id}
+                            budget={project.budget}
+                            category={project.category.name}
+                            key={project.id}
+                        />
+                    ))
+                }
             </Container>
         </div>
     );
