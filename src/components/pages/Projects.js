72a52b479094ee5387e3841e8de9124a4b0dcf1b
Kevin Souto <kevinsoutods@gmail.com>
2022-07-08 23:36:09 -0300

    Preparando o Dashboard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { useLocation } from 'react-router-dom';
 import Message from "../layout/Message";
+import Container from '../layout/Container';
+import LinkButton from '../layout/LinkButton';
 
-function Project() {
+import styles from './Projects.module.css';
+
+function Projects() {
 
     const location = useLocation();
     let message = '';
@@ -11,11 +15,19 @@ function Project() {
     }
 
     return (
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto"/>
+            </div>
+
             {message && <Message type="success" msg={message} />}
+            
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
         </div>
     );
 }
 
-export default Project;
+export default Projects;
